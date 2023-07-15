38151f7e0bb2aed8807f3f0acadfe9f56422fed8
Lokesh <lokeshkaushik2243@gmail.com>
2023-07-15 10:37:20 +0530

    employee icons done

--- src/components/employee/Reimbursement.tsx
+++ src/components/employee/Reimbursement.tsx
@@ -1,26 +1,32 @@
+import businessIcon from '/public/business-icon.svg';
+import mapIcon from "/public/map-icon.svg";
+import medicalIcon from "/public/medical_information_FILL0_wght300_GRAD0_opsz24 2.svg";
+import workSpaceIcon from "/public/workspace_premium_FILL0_wght300_GRAD0_opsz24 2.svg";
+import webhookIcon from "/public/webhook_FILL0_wght300_GRAD0_opsz24 2.svg";
+
 const Reimbursement = () => {
   return (
     <div className="pending-leaves leave-page w-full font-medium font-[Inter]">
       <p className="leave-page-heading">Please choose your reimbursement type</p>
       <ul className="flex flex-col px-[3px] mb-2">
         <li className="flex items-center gap-[6px] p-2 rounded-[4px] text-[15px] font-light hover:bg-[#7E55F1] hover:text-white">
-          <img src="/public/business-icon.svg" alt="business-icon" />
+          <img src={businessIcon} alt="business-icon" />
           Business and Conferences
         </li>
         <li className="flex items-center gap-[6px] p-2 rounded-[4px] text-[15px] font-light hover:bg-[#7E55F1] hover:text-white">
-          <img src="/public/map-icon.svg" alt="business-icon" />
+          <img src={mapIcon} alt="map-icon" />
           Travel and Accomodation
         </li>
         <li className="flex items-center gap-[6px] p-2 rounded-[4px] text-[15px] font-light hover:bg-[#7E55F1] hover:text-white">
-          <img src="/public/medical_information_FILL0_wght300_GRAD0_opsz24 2.svg" alt="business-icon" style={{color: 'black'}} />
+          <img src={medicalIcon} alt="medical-icon" style={{color: 'black'}} />
           Medical Expenses
         </li>
         <li className="flex items-center gap-[6px] p-2 rounded-[4px] text-[15px] font-light hover:bg-[#7E55F1] hover:text-white">
-          <img src="/public/workspace_premium_FILL0_wght300_GRAD0_opsz24 2.svg" alt="business-icon" />
+          <img src={workSpaceIcon} alt="workspace-icon" />
           Professional Certification
         </li>
         <li className="flex items-center gap-[6px] p-2 rounded-[4px] text-[15px] font-light hover:bg-[#7E55F1] hover:text-white">
-          <img src="/public/webhook_FILL0_wght300_GRAD0_opsz24 2.svg" alt="business-icon" />
+          <img src={webhookIcon} alt="business-icon" />
           Software requirement
         </li>
       </ul>
